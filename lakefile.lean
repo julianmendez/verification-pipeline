1dc2a41bc53d2e666989409db31c6afe7b663e76
Julian Mendez <julian.mendez@gmail.com>
2026-03-11 17:18:32 +0100

    Add lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,20 @@
+import Lake
+open Lake DSL
+
+package «tiles» where
+  -- add package configuration options here
+
+require batteries from git
+  "https://github.com/leanprover-community/batteries.git" @ "v4.28.0"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
+
+@[default_target]
+lean_lib «Soda» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «emotion» where
+  root := `Soda.tiles.emotion.main.Main
+
