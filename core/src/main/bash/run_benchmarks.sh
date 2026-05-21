0db689a818e5797f16a915849ecbd79aced63f8c
Julian Mendez <julian.mendez@gmail.com>
2026-05-21 22:34:48 +0200

    Update script to run the benchmarks

--- core/src/main/bash/run_benchmarks.sh
+++ core/src/main/bash/run_benchmarks.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
 #
-# This script runs an instance with several iterations.
-# Its only parameter is the input file.
+# This script runs an instance through several iterations.
+# Each iteration has a number of cyclically replicated repetitions
+# of the transitions in an instance.
+#
+# This script has only one parameter: the input file name.
+# If no input file name is provided, it will use a default value.
+#
 #
 
 
+## Initial number of cyclic replications.
 start=100
+
+## Increment between experiments.
 increment=100
+
+## Number of iterations.
 repetitions=10
+
+## Location of the binary file.
 application="./verify"
+
+## YAML input file with the instance.
 defaultInput="example.yaml"
+
+## Pattern of relevant time to retrieve.
 pattern="execution_time:"
+
+## Unit of time measured.
 milliseconds="ms"
 
 
